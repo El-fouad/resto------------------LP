1878ac2aa6568c55af39f7f546858793fc35c181
fouad <agri40@agri40.com>
2023-10-02 17:15:20 +0100

    config about route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import NavBar from "./component/NavBar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -14,7 +15,7 @@ function App() {
           <Route path="/" element={<Home/>} />
           <Route path="/Features" element={<h1>Features</h1>} />
           <Route path="/Blog" element={<h1>Blog</h1>} />
-          <Route path="/AbouUs" element={<h1>AbouUs</h1>} />
+          <Route path="/AbouUs" element={<About />} />
           <Route path="/*" element={<h1>error 404</h1>} />
         </Routes>
     </>
