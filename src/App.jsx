6c9478d33309c0d34caa3f655a80202662e2c883
fouad <agri40@agri40.com>
2023-10-04 17:40:05 +0100

    added blog compo

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import NavBar from "./component/NavBar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Blog from "./pages/Blog";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -14,7 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/Features" element={<h1>Features</h1>} />
-          <Route path="/Blog" element={<h1>Blog</h1>} />
+          <Route path="/Blog" element={<Blog />} />
           <Route path="/AbouUs" element={<About />} />
           <Route path="/*" element={<h1>error 404</h1>} />
         </Routes>
