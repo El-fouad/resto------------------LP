2a3cd1d4ff2d96ecbc4f406604146e6e93bc3600
fouad <agri40@agri40.com>
2023-10-05 12:28:02 +0100

    replace blog to chat

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import NavBar from "./component/NavBar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Blog from "./pages/Blog";
+import Chat from "./pages/Chat";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -15,7 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/Features" element={<h1>Features</h1>} />
-          <Route path="/Blog" element={<Blog />} />
+          <Route path="/Blog" element={<Chat />} />
           <Route path="/AbouUs" element={<About />} />
           <Route path="/*" element={<h1>error 404</h1>} />
         </Routes>
